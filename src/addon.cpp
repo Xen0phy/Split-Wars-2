7279dd4abf4475de6a7a0d16f6cf6bb5a5b90415
Xen0phy <xenophy@web.de>
2026-05-29 14:38:19 +0200

    Hotfix: loading screen timer

--- src/addon.cpp
+++ src/addon.cpp
@@ -40,7 +40,7 @@ extern "C" __declspec(dllexport) AddonDefinition_t* GetAddonDef()
     AddonDef.Version.Major      = 0;
     AddonDef.Version.Minor      = 17;
     AddonDef.Version.Build      = 0;
-    AddonDef.Version.Revision   = 0;
+    AddonDef.Version.Revision   = 1;
     AddonDef.Author             = "Xenophy.2716";
     AddonDef.Description        = "A speedrun timer with coordinate-based triggers.";
     AddonDef.Load               = AddonLoad;            // Called once when the addon is loaded
